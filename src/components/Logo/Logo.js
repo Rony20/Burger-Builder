d83c644e3d919153d099f018cd8d8aafeb3c6efc
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-05-27 13:03:03 +0530

    Toggle Button is added in toolbar to show sidedrawer conditionally. For performance improvement modal is checked because every time we add items in burger it should not update. It should only update when order now button is clicked. for that shouldComponent update method is added in it. Because of it order summary will alos not update. Because Modal is wrapping element of it.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,8 +3,7 @@ import React from 'react'
 import classes from "./Logo.module.css"
 
 import burgerLogo from "../../assets/Images/burger-logo.png"
-
-const logo = () => {
+const logo = (props) => {
     return(
         <div className={classes.Logo}>
             <img src={burgerLogo} alt="MyBurger"/>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,17 +2,20 @@ import React from 'react'
 
 import classes from "./Toolbar.module.css"
 import NavigationItems from "../NavigationItems/NavigationItems"
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle"
 
 import Logo from "../../Logo/Logo"
 
-const toolbar = () => {
+const toolbar = (props) => {
     return(
         <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo />
-            <nav><NavigationItems/></nav>
+            <DrawerToggle clicked={props.drwawerToggleClicked}/>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <nav className={classes.DesktopOnly}><NavigationItems/></nav>
         </header>
     )
 }
 
-export default toolbar
+export default toolbar  
